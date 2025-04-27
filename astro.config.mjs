8f39baac10e35eae2eef9772d5e546739aca8a8f
HenryCBQ <henryalexis1999@gmail.com>
2025-04-27 16:41:37 -0500

    Remove @astrojs/vercel dependency from package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  adapter: vercel(),
-  site: "https://ghan-carlos-land.vercel.app/"
+  site: "https://ghan-carlos-land.pages.dev/"
 });
