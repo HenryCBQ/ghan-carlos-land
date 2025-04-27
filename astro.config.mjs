8d4be8a53083a7f7c304de4b1db92b8d62dd3535
HenryCBQ <henryalexis1999@gmail.com>
2025-04-27 17:37:01 -0500

    Add Cloudflare adapter and update .gitignore; enable prerendering for blog post pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  site: "https://ghan-carlos-land.pages.dev/"
+  site: "https://ghan-carlos-land.pages.dev/",
+  adapter: cloudflare({
+    imageService: 'cloudflare'
+  }),
+  output: 'server',
+  experimental: {
+    session: true
+  }
 });
