00dd3e3949b4bd20778c71bb8229a1bac13356f9
HenryCBQ <henryalexis1999@gmail.com>
2025-04-27 19:28:13 -0500

    Remove Cloudflare adapter from configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  site: "https://ghan-carlos-land.pages.dev/",
-  output: 'server',
-  adapter: cloudflare({
-    imageService: 'passthrough',
-  }),
-  experimental: {
-    session: true
-  }
+  trailingSlash: 'never',
+  site: "https://ghan-carlos-land.pages.dev",
 });
